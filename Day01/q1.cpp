265a4d128c59e81ad0f293cc6ff4cc872f47c0cf
1234abhibisht <abhibisht129@gmail.com>
2025-08-28 17:36:14 +0530

    Create q1.cpp
    1st day 1st question

---
+++ Day01/q1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sum(int a, int b) {
+    int z=a+b;
+    printf("Sum of %d and %d is : %d",a, b, z);
+    return z;
+}
+int main(int argc, char *argv[]) {
+    int a;
+    printf("Enter your first number : ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter your second number : ");
+    scanf("%d",&b);
+    sum(a,b);
+    return 0;
+}
