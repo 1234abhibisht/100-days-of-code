90be57515caacfe78520a66d960171e712b9f076
1234abhibisht <abhibisht129@gmail.com>
2025-09-03 08:15:43 +0530

    Create EvenOddChecker.c

---
+++ Day06/EvenOddChecker.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void EvenOdd(int n) {
+    if(n%2==0) printf("%d is Even",n);
+    else printf("%d is Odd",n);
+}
+int main() {
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    EvenOdd(n);
+    return 0;
+}
