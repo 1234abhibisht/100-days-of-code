ce06d35e82a530a581abca62c3f7449a4ab7e799
1234abhibisht <abhibisht129@gmail.com>
2025-09-03 09:04:13 +0530

    Create PositiveNegativeChecker.c

---
+++ Day06/PositiveNegativeChecker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int PositiveNegative(int n) {
+    if(n>0) printf("%d is positive",n);
+    else if(n<0) printf("%d is negative",n);
+    else printf("%d is zero");
+}
+int main() {
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    PositiveNegative(n);
+    return 0;
+}
