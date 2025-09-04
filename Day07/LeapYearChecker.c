69a66aa117028f5977cf7dadff705b8f6c9b57de
1234abhibisht <abhibisht129@gmail.com>
2025-09-04 13:16:36 +0530

    Create LeapYearChecker.c

---
+++ Day07/LeapYearChecker.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int year(int x) {
+    if(x%4==0) printf("%d is a leap year",x);
+    else printf("%d is not a leap year",x);
+}
+int main() {
+    int x;
+    printf("Enter year : ");
+    scanf("%d",&x);
+    year(x);
+    return 0;
+}
