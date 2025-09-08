7ff033c0d267fb1eefbd5d8cf03a80c4516e1172
1234abhibisht <abhibisht129@gmail.com>
2025-09-08 14:00:42 +0530

    Create LibraryLateSubmissionFine.c

---
+++ Day12/LibraryLateSubmissionFine.c
@@ -0,0 +1,25 @@
+/* fine will be of ruppees 2 for one day late submission and will increase
+   by ruppees 4 for more than one day 
+   EX - for one day late submission - 2 ruppees
+        for two day late submission - 6 ruppees
+        for three day - 10 ruppees 
+        for four day - 14 ruppees*/
+   
+#include <stdio.h>
+void fine(int days) {
+    if(days == 1) {
+        printf("Fine is of ruppees : 2");
+    }
+    else if(days > 1 && days < 31) {
+        int x = days * 4 - 2;
+        printf("Fine is of ruppees : %d",x);
+    }
+    else printf("Membership cancelled");
+}   
+int main() {
+    int days;
+    printf("Enter number of days you are late for submission : ");
+    scanf("%d",&days);
+    fine(days);
+    return 0;
+}
