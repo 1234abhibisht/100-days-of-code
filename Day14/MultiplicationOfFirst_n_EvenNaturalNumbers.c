7b1df432d71c5e287b532770e7a9d76f9580dbd5
1234abhibisht <abhibisht129@gmail.com>
2025-09-10 08:20:17 +0530

    Create MultiplicationOfFirst_n_EvenNaturalNumbers.c

---
+++ Day14/MultiplicationOfFirst_n_EvenNaturalNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void multiplication(int n) {
+    int mul = 1;
+    for(int i = 2; i <= 2 * n; i++) {
+        if(i % 2 == 0) mul = mul * i;
+    }
+    printf("multiplication of first %d even natural numbers is : %d",n,mul);
+}
+int main() {
+    int n;
+    printf("Enter value of n : ");
+    scanf("%d",&n);
+    multiplication(n);
+    return 0;
+}
