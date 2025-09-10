4c60a6a38d747966131ba40c8d67d3300ca739bf
1234abhibisht <abhibisht129@gmail.com>
2025-09-10 09:42:01 +0530

    Update GivingGradesBasedOnPercentage.c

--- Day09/GivingGradesBasedOnPercentage.c
+++ Day09/GivingGradesBasedOnPercentage.c
@@ -4,11 +4,23 @@ int main() {
     float percentage;
     printf("Enter your percentage : ");
     scanf("%f",&percentage);
-    if(percentage > 90 && percentage <= 100) printf("You got A grade");
-    else if(percentage > 80 && percentage <= 90) printf("You got B grade");
-    else if(percentage > 70 && percentage <= 80) printf("You got C grade");
-    else if(percentage > 60 && percentage <= 70) printf("You got D grade");
-    else if(percentage > 50 && percentage <= 60) printf("You got E grade");
-    else printf("You are fail");
+    if(percentage > 90 && percentage <= 100) {
+        printf("You got A grade");
+    }
+    else if(percentage > 80 && percentage <= 90) {
+         printf("You got B grade");
+    }
+    else if(percentage > 70 && percentage <= 80) {
+        printf("You got C grade");
+    }
+    else if(percentage > 60 && percentage <= 70) {
+        printf("You got D grade");
+    }
+    else if(percentage > 50 && percentage <= 60) {
+        printf("You got E grade");
+    }
+    else {
+        printf("You are fail");
+    }
     return 0;
 }
