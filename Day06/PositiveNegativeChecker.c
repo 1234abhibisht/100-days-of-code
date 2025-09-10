a7c85bec3da7c7b04d7a9099166439bbe9517cec
1234abhibisht <abhibisht129@gmail.com>
2025-09-10 09:35:21 +0530

    Update PositiveNegativeChecker.c

--- Day06/PositiveNegativeChecker.c
+++ Day06/PositiveNegativeChecker.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int PositiveNegative(int n) {
-    if(n>0) printf("%d is positive",n);
-    else if(n<0) printf("%d is negative",n);
-    else printf("%d is zero");
+    if(n>0) {
+        printf("%d is positive",n);
+    }
+    else if(n<0) {
+        printf("%d is negative",n);
+    }
+    else {
+        printf("%d is zero");
+    }
 }
 int main() {
     int n;
