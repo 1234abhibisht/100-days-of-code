343599f4f42538151eefd1841eea5f0a96e5e7ff
1234abhibisht <abhibisht129@gmail.com>
2025-09-18 17:43:57 +0530

    Create NumberTriangleReverseRow.c

---
+++ Day25/NumberTriangleReverseRow.c
@@ -0,0 +1,24 @@
+/* print    5
+            4 5
+            3 4 5
+            2 3 4 5
+            1 2 3 4 5*/
+
+#include <stdio.h>
+int triangle(int n) {
+    for(int i = n; i >= 1; i--) {  /* horizontal rows */
+        int x = i;
+        for(int j = n; j >= i; j--) {  /* vertical columns */
+            printf("%d ",x);
+            x++;
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of rows : ");
+    scanf("%d",&n);
+    triangle(n);
+    return 0;
+}
