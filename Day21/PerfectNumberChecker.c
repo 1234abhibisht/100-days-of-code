6f924b458ab9d112296275aed85a65f95f1ae4ad
1234abhibisht <abhibisht129@gmail.com>
2025-09-19 19:59:07 +0530

    Update PerfectNumberChecker.c

--- Day21/PerfectNumberChecker.c
+++ Day21/PerfectNumberChecker.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-void factors(int n) {
+void factors(int n)
+{
     int j = 0;
     int arr[n - 1];
-    for(int i = 1; i <= n - 1; i++) {
-        if(n % i == 0) {
+    for (int i = 1; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
             arr[j] = i;
             j++;
         }
     }
     int sum = 0;
-    for(int k = 0; k <= 2; k++) {
+    for (int k = 0; k <= 2; k++)
+    {
         sum = sum + arr[k];
     }
-    if(sum == n) {
-        printf("%d is a perfect number",n);
+    if (sum == n)
+    {
+        printf("%d is a perfect number", n);
     }
-    else {
-        printf("%d is not a perfect number",n);
+    else
+    {
+        printf("%d is not a perfect number", n);
     }
+    return;
 }
-int main() {
+int main()
+{
     int n;
     printf("Enter a number : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     factors(n);
     return 0;
 }
