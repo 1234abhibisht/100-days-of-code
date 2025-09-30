0197043b8f749efc73a146ddf785b0a218a2fa3d
1234abhibisht <abhibisht129@gmail.com>
2025-09-30 19:55:27 +0530

    Create TakeInputAndOutputOfMatrix.c

---
+++ Day36/TakeInputAndOutputOfMatrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+    int m;
+    printf("Enter number of columns : ");
+    scanf("%d", &m);
+    int arr[n][m];
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = 0; j <= m - 1; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("Matrix is : \n");
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = 0; j <= m - 1; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
