70002eed34f997a8de568ab7850e347f3a7fba53
1234abhibisht <abhibisht129@gmail.com>
2025-10-01 14:09:08 +0530

    Create TransposeOf_m_n_Matrix.c

---
+++ Day37/TransposeOf_m_n_Matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+    int m;
+    printf("Enter number of columns : ");
+    scanf("%d", &m);
+    int arr[n][m];
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = 0; j <= m - 1; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = i + 1; j <= m - 1; j++)
+        {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+    /* after transpose rows will be columns and columns will be rows */
+    printf("Transpose of matrix is : \n");
+    for (int i = 0; i <= m - 1; i++)
+    {
+        for (int j = 0; j <= n - 1; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
