551cb3ba4742cf4bb07235e0a482e0b786771c70
1234abhibisht <abhibisht129@gmail.com>
2025-10-02 12:51:53 +0530

    Update AddTwoMatricesUsingThirdMatrix.c

--- Day38/AddTwoMatricesUsingThirdMatrix.c
+++ Day38/AddTwoMatricesUsingThirdMatrix.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter number of rows : ");
-    scanf("%d", &n);
-    int m;
-    printf("Enter number of columns : ");
-    scanf("%d", &m);
-    int arr[n][m];
-    for (int i = 0; i <= n - 1; i++)
+    int arr1[3][3];
+    printf("Enter first matrix : \n");
+    for (int i = 0; i <= 2; i++)
     {
-        for (int j = 0; j <= m - 1; j++)
+        for (int j = 0; j <= 2; j++)
         {
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &arr1[i][j]);
         }
     }
-    /* create an array to store sum of each row */
-    int arrSum[n];
-    int x = 0; /* index of array */
-    /* sum of each row of matrix */
-    for (int i = 0; i <= n - 1; i++)
+    int arr2[3][3];
+    printf("Enter second matrix : \n");
+    for (int i = 0; i <= 2; i++)
     {
-        int sum = 0;
-        for (int j = 0; j <= m - 1; j++)
+        for (int j = 0; j <= 2; j++)
         {
-            sum = sum + arr[i][j];
+            scanf("%d", &arr2[i][j]);
         }
-        arrSum[x] = sum;
-        x++;
     }
-    printf("Array containing sum of each row of matrix is : ");
-    for (int i = 0; i <= n - 1; i++)
+    for (int i = 0; i <= 2; i++)
     {
-        printf("%d ", arrSum[i]);
+        for (int j = 0; j <= 2; j++)
+        {
+            arr1[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+    printf("Sum of matrix 1 and matrix 2 is : \n");
+    for (int i = 0; i <= 2; i++)
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            printf("%d ", arr1[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
