57f0532d6a6a862875643c7eac68647651a30eb9
1234abhibisht <abhibisht129@gmail.com>
2025-10-16 22:44:10 +0530

    Create FirstRepeatingLowerCaseAlphabetInString.c

---
+++ Day46/FirstRepeatingLowerCaseAlphabetInString.c
@@ -0,0 +1,47 @@
+/* we are given a string, find first lower case alphabet which is repeating twice or more */
+
+#include <stdio.h>
+#include <stdbool.h>
+int firstRepeatingElement(char str[], int size)
+{
+    char x;
+    bool flag = false;
+    for (int i = 0; i <= size - 1; i++)
+    {
+        for (int j = i + 1; j <= size - 1; j++)
+        {
+            if (str[i] == str[j])
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    x = str[i];
+                    return x; /* if repeating alphabet exists in array */
+                }
+            }
+        }
+    }
+    return '\0';
+}
+int main()
+{
+    char str[40];
+    printf("Enter your string : ");
+    scanf("%[^\n]s", &str);
+    int i = 0;
+    int size = 0;
+    while (str[i] != '\0')
+    {
+        size++;
+        i++;
+    }
+    char result = firstRepeatingElement(str, size);
+    if (result == '\0')
+    {
+        printf("No repeating lower case alphabet found");
+    }
+    else
+    {
+        printf("First repeating lower case alphabet is %c", result);
+    }
+    return 0;
+}
